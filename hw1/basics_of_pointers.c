ce3f69bc700dc3bf8af655b4c6b1ad4528fa3fcf
lusyasargsyan <lusya_sargsyan@edu.aua.am>
2026-09-09 05:43:22 +0000

    added prints

--- hw1/basics_of_pointers.c
+++ hw1/basics_of_pointers.c
@@ -4,8 +4,12 @@ int main () {
 	int x = 10;
 	int* p = &x;
 
-	printf("using int variable %d\n", x);
-	printf("using pointer x = %d\n", *p);
+	printf("value using int variable %d\n", x);
+	printf("value using pointer x = %d\n", *p);
+
+	printf("address using int variable %p\n", &x);
+        printf("address using pointer x = %p\n", p);
+	printf("&x == p : %b\n", &x == p);
 
 	*p = 7;
 	printf("after update using int variable %d\n", x);
